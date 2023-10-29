d8729bea385538a87bb655bf6956c1a865a8673a
Gaesol2 <de_peimare@naver.com>
2023-10-29 16:04:46 +0900

    오라클 데이터베이스 이름 수정 및 review 작업

--- src/main/java/com/shop/dessertFairy/review/dto/ReviewDTO.java
+++ src/main/java/com/shop/dessertFairy/review/dto/ReviewDTO.java
@@ -1,13 +1,13 @@
 package com.shop.dessertFairy.review.dto;
 
 public class ReviewDTO {
-	private String r_no;
+	private int r_no;
 	private String r_subject;
 	private String r_content;
 	private String r_regdate;
-	private String r_readcount;
+	private int r_readcount;
 	private String r_passwd;
-	private String r_star ;
-	private String r_image    ;
-	private String m_id       ;
+	private String r_star;
+	private String r_image;
+	private int m_id;
 }
---
+++ src/main/java/com/shop/dessertFairy/review/service/ReviewServiceImpl.java
@@ -0,0 +1,5 @@
+package com.shop.dessertFairy.review.service;
+
+public class ReviewServiceImpl implements ReviewService {
+
+}
