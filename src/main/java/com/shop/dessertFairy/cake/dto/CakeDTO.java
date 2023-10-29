488f64d83e831cc5e7d28e22a94de3c4d3ea3a51
bockson <whguswls8421@naver.com>
2023-10-29 17:07:28 +0900

    주문제작 케이크 캘린더 구현(예약은 x)

---
+++ src/main/java/com/shop/dessertFairy/cake/dto/CakeDTO.java
@@ -0,0 +1,23 @@
+package com.shop.dessertFairy.cake.dto;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class CakeDTO {
+	private int c_no;
+	private String c_sheet;
+	private String c_incream;
+	private String c_outcream;
+	private String c_size;
+	private String c_letter; 
+	private String c_image;
+	private String c_request;
+	private String c_price;
+	private String c_regdate;
+	private String c_state;
+	private String c_pickupdate;
+	private String m_id;
+
+}
---
+++ src/main/java/com/shop/dessertFairy/cake/service/CakeServiceImpl.java
@@ -0,0 +1,16 @@
+package com.shop.dessertFairy.cake.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.shop.dessertFairy.cake.dao.CakeDAO;
+@Service
+public class CakeServiceImpl implements CakeService {
+	@Autowired
+	CakeDAO cakeDao;
+
+	
+	
+
+}
