fe30db5920faf9651d4947c3578c4b918be5b3dc
Gaesol2 <de_peimare@naver.com>
2023-10-31 11:18:28 +0900

    포토 리뷰 컨트롤러 주석 처리

--- src/main/java/com/shop/dessertFairy/review/web/ReviewController.java
+++ src/main/java/com/shop/dessertFairy/review/web/ReviewController.java
@@ -30,8 +30,6 @@ public class ReviewController {
    public String PhotoList (HttpServletRequest request, HttpServletResponse response,
          Model model, ReviewDTO rdto) {
       
-      
-      
       String contentsJsp = "/custom/review/ReviewList";
       
       model.addAttribute("contentsJsp",contentsJsp);
@@ -66,7 +64,7 @@ public class ReviewController {
 		   url = "login";
 	   }
 	   
-
+	   //데이터 저장
 	   model.addAttribute("contentsJsp",contentsJsp);
 	   model.addAttribute("msg",msg);
 	   model.addAttribute("url",url);
@@ -80,50 +78,46 @@ public class ReviewController {
    @RequestMapping("/reviewWriteProc")
    public String ReviewWriteProc (HttpServletRequest request, HttpServletResponse response,
          Model model, ReviewDTO rdto, @RequestParam("image2") MultipartFile file) {
+
+	   //변수 선언
+	   String msg = null;
+	   String url = "/reviewList";
+	   
+	   //세션 받아오기
+	   HttpSession session = request.getSession();
+	   MemberDTO sdto = (MemberDTO) session.getAttribute("ssKey");
+
+	   //rdto.setM_id(세션) 세션에서 member_id를 빼서 rdto에 넣어주기
+	   rdto.setM_id(sdto.getM_id());
+
+	   //이미지 경로 저장
+	   rdto.setR_path(resourcesLocation);
+
+	   //결과 처리
+	   int result = reviewService.reviewWrite(rdto, file);
+
+	   //결과에 따른 메세지 출력
+	   if(result>0) {
+		   msg = "포토리뷰가 등록되었습니다.";
+	   } else {
+		   msg = "포토리뷰 등록에 실패했습니다.";
+	   }
       
-      System.out.println("글쓰기프록");
-      String msg = null;
-      String url = "/reviewList";
+	   //경로 및 메세지 저장
+	   model.addAttribute("url",url);
+	   model.addAttribute("msg",msg);
       
-      //세션 받아오기
-      HttpSession session = request.getSession();
-      MemberDTO sdto = (MemberDTO) session.getAttribute("ssKey");
-      rdto.setM_id(sdto.getM_id());
-      
-      //rdto.setM_id(세션) 세션에서 member_id를 빼서 rdto에 넣어주기
-      
-      
-      rdto.setR_path(resourcesLocation);
-      
-      System.out.println(rdto.getR_path());
-      System.out.println("file ====== : " + file);
-      
-      int result = reviewService.reviewWrite(rdto, file);
-      
-      System.out.println("result======"+result);
-      
-      if(result>0) {
-         msg = "포토리뷰가 등록되었습니다.";
-      } else {
-         msg = "포토리뷰 등록에 실패했습니다.";
-      }
-      
-      model.addAttribute("url",url);
-      model.addAttribute("msg",msg);
-      
-      //세션 저장해주기
-      
-      return "MsgPage";
+	   //세션 저장
+	   session.setAttribute("ssKey", sdto);
+	   
+	   return "MsgPage";
    }
 
    @RequestMapping("/reviewContent")
    public String ReviewContent (HttpServletRequest request, HttpServletResponse response,
          Model model, ReviewDTO rdto) {
       
-      
-      
       String contentsJsp = "/custom/review/ReviewContent";
-      
       
       model.addAttribute("contentsJsp",contentsJsp);
       
