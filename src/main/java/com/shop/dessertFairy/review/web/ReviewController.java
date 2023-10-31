112318008d517548dba2485c10a86eadb5690e53
Gaesol2 <de_peimare@naver.com>
2023-10-31 11:04:40 +0900

    포토리뷰 글쓰기 구현

--- src/main/java/com/shop/dessertFairy/review/web/ReviewController.java
+++ src/main/java/com/shop/dessertFairy/review/web/ReviewController.java
@@ -8,11 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.shop.dessertFairy.member.dto.MemberDTO;
 import com.shop.dessertFairy.review.dto.ReviewDTO;
 import com.shop.dessertFairy.review.service.ReviewService;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class ReviewController {
@@ -40,20 +42,39 @@ public class ReviewController {
    @RequestMapping("/reviewWrite")
    public String ReviewWrite (HttpServletRequest request, HttpServletResponse response,
          Model model, ReviewDTO rdto) {
+
+	   //HttpSession 세션 객체 생성 및 세션 정보 받아오기
+	   HttpSession session = request.getSession();
+
+	   //ssKey 세션에 있는 정보를 MemberDTO 타입의 sdto에 저장
+	   MemberDTO sdto = (MemberDTO) session.getAttribute("ssKey");
+
+	   //변수 선언
+	   String url = null;
+	   String msg = null;
+	   String page = null;
+	   String contentsJsp = null;
+	   
+	   //세션이 없으면 로그인 먼저 하라고 로그인 페이지로 보내기
+	   if(sdto!=null) {
+		   contentsJsp = "/custom/review/ReviewWrite";
+		   page = "Main";
+		   model.addAttribute("m_id",sdto.getM_id());
+	   } else {
+		   msg = "로그인이 필요합니다.";
+		   page = "MsgPage";
+		   url = "login";
+	   }
+	   
+
+	   model.addAttribute("contentsJsp",contentsJsp);
+	   model.addAttribute("msg",msg);
+	   model.addAttribute("url",url);
       
-      //HttpSession 세션 받아오기
-      
-      //세션이 없으면 로그인 먼저 하라고 로그인 페이지로 보내기
-      
-      String contentsJsp = "/custom/review/ReviewWrite";
-      String msg = null;
-      
-      model.addAttribute("contentsJsp",contentsJsp);
-      model.addAttribute("msg",msg);
-      
-      //세션 저장해주기
-      
-      return "Main";
+	   //세션 저장해주기
+	   session.setAttribute("ssKey", sdto);
+	   
+	   return page;
    }
 
    @RequestMapping("/reviewWriteProc")
@@ -65,8 +86,12 @@ public class ReviewController {
       String url = "/reviewList";
       
       //세션 받아오기
+      HttpSession session = request.getSession();
+      MemberDTO sdto = (MemberDTO) session.getAttribute("ssKey");
+      rdto.setM_id(sdto.getM_id());
       
       //rdto.setM_id(세션) 세션에서 member_id를 빼서 rdto에 넣어주기
+      
       
       rdto.setR_path(resourcesLocation);
       
