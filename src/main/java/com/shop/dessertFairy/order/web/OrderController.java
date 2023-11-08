45112467fefe5b0c0c7f63d10f395cc4cb74f345
Gaesol2 <de_peimare@naver.com>
2023-11-08 18:13:09 +0900

    order 수정중

--- src/main/java/com/shop/dessertFairy/order/web/OrderController.java
+++ src/main/java/com/shop/dessertFairy/order/web/OrderController.java
@@ -2,11 +2,15 @@ package com.shop.dessertFairy.order.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.shop.dessertFairy.member.dto.MemberDTO;
+import com.shop.dessertFairy.order.dto.OrderDTO;
 import com.shop.dessertFairy.wrapper.OrderWrapper;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
@@ -16,8 +20,10 @@ public class OrderController {
 	OrderWrapper orderWrapper;
 	
 	@RequestMapping("orderProc")
-	public String OrderProc(HttpServletRequest request) {
-		
+	public String OrderProc(HttpServletRequest request, HttpServletResponse response,
+			Model model, OrderDTO odto) {
+		HttpSession session = request.getSession();
+		MemberDTO sdto = (MemberDTO) session.getAttribute("ssKey");
 		
 		
 		return "Main";
