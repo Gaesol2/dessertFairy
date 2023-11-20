5c1e8bd2dad6c65d21314461b082ee7a5c2d6fa5
Gaesol2 <de_peimare@naver.com>
2023-11-20 14:24:13 +0900

    마이페이지 수정 완료

--- src/main/resources/static/script/mypage.js
+++ src/main/resources/static/script/mypage.js
@@ -24,10 +24,11 @@
 	
 	function update(flag){
 		//패스워드 확인하기
-		
+		openWin = window.open("/pwCheck","pwCheak",
+		"width=400, height=150, toolbar=no, location=no, menubar=no, resizable=no, scrollbars=no")
 		if(flag=='u'){
 			$("#upForm").attr("action", "/memUpForm");
-		}else{
+		}else if(flag=='d'){
 			$("#upForm").attr("action", "/memDelete");	
 			var yn = confirm("정말 탈퇴하시겠습니까?\n 구매이력과 포인트가 소멸됩니다.")
 			if(yn==false){
@@ -37,14 +38,22 @@
 	}
 		
 		
-	 function checkPw(){
-	 spw = $('input[name=update]').val();
-	 upw = prompt("비밀번호를 입력하시오");
-	 if(upw==spw){
-	    return true;
-	 }else{
-	    return false;
-	 }
+	function setParentText(){
+		 var pw1 = $('#pwck').val() //popup에서 받은 pass
+		 if(pw1==null || pw1.length==0){
+			 alert('패스워드를 입력하세요');
+			$('#pwck').focus();
+			 return false;
+		 }
+		 var pw2 = $("#pw", opener.document).val(); //opener의 pw
+		 if(pw1==pw2){
+			$("#upForm", opener.document).submit();
+			this.window.close();
+		}else{
+			alert('비밀번호 오류');
+			this.window.close();
+		}
+		
 	}
 
 	 
